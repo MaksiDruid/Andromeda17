5e3573f2ffc2f1ba3e2fd8f252e0e4e0ac249f29
MaksiDruid <sergeykidv@gmail.com>
2024-11-09 18:28:24 +0300

    setup generation

--- Project/Assets/Scripts/RoomMover.cs
+++ Project/Assets/Scripts/RoomMover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RoomMover : MonoBehaviour
 {
+    [SerializeField] private float shiftMax;
+    [SerializeField] private float speed;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Awake()
@@ -14,6 +16,13 @@ public class RoomMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = -Vector3.forward;
+        rb.velocity = -Vector3.forward * speed;
+
+        if (transform.position.z <= -8)
+        {
+            float horShift = Random.Range(-shiftMax, shiftMax);
+            float verShift = Random.Range(-shiftMax, shiftMax);
+            transform.position = new Vector3(horShift, verShift, 16);
+        }
     }
 }
