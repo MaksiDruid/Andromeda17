252fd996ded7a4a47c3156316b419f47c87b7be6
MaksiDruid <sergeykidv@gmail.com>
2024-11-09 16:18:18 +0300

    pipe added

--- Project/Assets/Scripts/Pipe.cs
+++ Project/Assets/Scripts/Pipe.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomMover : MonoBehaviour
+public class Pipe : MonoBehaviour
 {
-    private Rigidbody rb;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = -Vector3.forward;
+        
     }
 }
