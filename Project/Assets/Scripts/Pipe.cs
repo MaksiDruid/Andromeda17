4e14669f707e0e39a4cee2bd3afc8b32e0f44e5d
MaksiDruid <sergeykidv@gmail.com>
2024-11-23 16:15:42 +0300

    setup moving

--- Project/Assets/Scripts/Pipe.cs
+++ Project/Assets/Scripts/Pipe.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour
 {
+    public bool firstRoom;
+    Rigidbody body;
+    int speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = new Vector3(-speed, 0, 0);
+
+        if (transform.position.x < -5)
+        {
+            transform.position = new Vector3(20, 0, 0);
+            if (firstRoom == true)
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
--- Project/Assets/Scripts/PipeGenerator.cs
+++ Project/Assets/Scripts/PipeGenerator.cs
@@ -10,7 +10,16 @@ public class PipeGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var pos in positions)
+        {
+            int randomNum = Random.Range(0, pipes.Count);
+            while (pipes[randomNum].gameObject.activeSelf == true)
+            {
+                randomNum = Random.Range(0, pipes.Count);
+            }
+            pipes[randomNum].position = pos.position;
+            pipes[randomNum].gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
