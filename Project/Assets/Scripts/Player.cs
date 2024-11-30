fc8a48b6ba6fc72c116c93a777319a7893f93d6d
MaksiDruid <sergeykidv@gmail.com>
2024-11-30 14:35:41 +0300

    player added

---
+++ Project/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public Transform camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 input = new Vector3 (horizontal, vertical, 0);
+        
+        rb.velocity = input * speed;
+    }
+}
